7880005f7131a147685d2486fc9c7c3f88695508
Matt Burrough <mburrough@gmail.com>
2011-11-30 00:10:32 -0500

    Fixed a warning in TaskExecutorStatus

--- mp4/src/main/java/edu/illinois/cs/mapreduce/TaskExecutorStatus.java
+++ mp4/src/main/java/edu/illinois/cs/mapreduce/TaskExecutorStatus.java
@@ -3,15 +3,16 @@ package edu.illinois.cs.mapreduce;
 import java.io.Serializable;
 
 public class TaskExecutorStatus implements Serializable {
-	private final double cpuUtilization;
-	private final int queueLength;
-	private final double throttle;
+
+    private static final long serialVersionUID = -3981153742009125690L;
+    
+    private final double cpuUtilization;
+    private final int queueLength;
+    private final double throttle;
 	
-	public TaskExecutorStatus(double cpuUtil, int length, double throttle) {
-		this.cpuUtilization = cpuUtil;
-		this.queueLength = length;
-		this.throttle = throttle;
-	}
-	
-	
+    public TaskExecutorStatus(double cpuUtil, int length, double throttle) {
+        this.cpuUtilization = cpuUtil;
+	this.queueLength = length;
+	this.throttle = throttle;
+    }
 }
